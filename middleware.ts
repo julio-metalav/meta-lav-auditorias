80394c360578a4dde9dec48b4a115ca453e6d083
Julio Cesar <metalavbrasil@gmail.com>
2026-01-07 22:44:10 -0400

    chore: add middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -37,3 +37,6 @@ export function middleware(req: NextRequest) {
 export const config = {
   matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
