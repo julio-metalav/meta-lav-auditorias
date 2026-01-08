5cf26dc38960aec15f944e3b1a2de887c7a45728
Julio Cesar <metalavbrasil@gmail.com>
2026-01-07 22:57:51 -0400

    fix: middleware config duplicated

--- middleware.ts
+++ middleware.ts
@@ -37,6 +37,3 @@ export function middleware(req: NextRequest) {
 export const config = {
   matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
-export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
-};
