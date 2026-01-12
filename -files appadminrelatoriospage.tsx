acc5288ccdeace86108f0db4bf948663ce07060c
Julio Cesar <metalavbrasil@gmail.com>
2026-01-11 21:56:30 -0400

    fix: remove accidental -files*.tsx committed output

--- -files appadminrelatoriospage.tsx
+++
@@ -1,1 +0,0 @@
-[33mc88640a[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m fix: rpc criar_auditorias_mensais sem argumentos
